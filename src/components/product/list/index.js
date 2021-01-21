9a9b4759b8aee065d9a1add8e3577fdc2997e64d
jules <jsoaresandrie@gmail.com>
2021-01-21 19:51:52 -0300

    finished checkout page

---
+++ src/components/product/list/index.js
@@ -0,0 +1,31 @@
+import './styles.css';
+
+const Product = () => {
+  return (
+    <div className="product-list col-12">
+      <div className="row">
+        <div className="col-3">
+          <img 
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRGs-S1S8UgKAbRjFuJeVs85YdZhmTWiXV5_SBebuh5BilTtw72dkuWsl86w&usqp=CAc" 
+            className="img-fluid" />
+        </div>
+        <div className="col-6">
+          <h6>
+            <label className="badge badge-primary">R$ 30,00</label>
+          </h6>
+          <small>
+            <b>
+              Ração Úmida Nestlé Purina Dog ChoW Extra Life Sachê Carne para
+              Cães Filhotes - 100g
+            </b>
+          </small>
+        </div>
+        <div className="col-3">
+          <button className="btn btn-secondary rounded-circle">-</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Product;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 
 import './styles/global.css'
 import Cadastro from './pages/cadastro';
+import Checkout from './pages/checkout';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Cadastro />
+    <Checkout />
   </React.StrictMode>,
   document.getElementById('root')
 );
